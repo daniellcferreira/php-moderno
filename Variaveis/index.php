6ef9b693321ddfe7fb3820bb2cff10dd7b36ec0d
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2023-11-30 16:28:35 -0300

    create variaveis\index.php

---
+++ Variaveis/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de Variaveis</title>
+</head>
+<body>
+    <?php 
+        $nome = "Daniel";
+        $sobrenome = "Ferreira";
+        $nome = "Geraldo";
+        const PAIS = "Brasil";
+        echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+
+    ?>    
+</body>
+</html>
