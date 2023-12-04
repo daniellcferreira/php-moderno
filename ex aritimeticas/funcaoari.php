9ffe9763283febbd4148b0a27d1c76ac83fa10d8
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2023-12-04 16:14:38 -0300

    create exAritimeticas

---
+++ ex aritimeticas/funcaoari.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções Aritimeticas</title>
+</head>
+<body>
+    <?php 
+        $r = abs(500);
+        print "A resposta é $r";
+
+        $r = base_convert(254, 10, 16);
+        print "A resultado é $r";
+
+        $r = intdiv(5, 2);
+        print "A resultado é $r";
+
+        $r = pow(5, 2);
+        print "A resultado é $r";
+
+        $r = sqrt(81);
+        print "A resultado é $r";
+    ?>    
+</body>
+</html>
