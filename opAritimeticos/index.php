2a519bed94803a111a886fa66866897083a910b8
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2023-12-05 16:36:12 -0300

    atualização de exercicios

---
+++ opAritimeticos/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritimeticos</title>
+</head>
+<body>
+    <?php 
+        $r = "2" + "2";
+        var_dump($r);
+
+        $r = "2" . "2";
+        var_dump($r);
+
+        $r = 5 ** 2;
+        var_dump($r);
+    ?>   
+</body>
+</html>
